ae04f2f27fbf274055718ad8cc9be9c8b8c1aa77
Sainath <bagadeesai4@gmail.com>
2026-01-22 22:41:06 +0530

    Remove Experience section from UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,6 @@ import Header from './components/Header';
 import Hero from './components/Hero';
 import About from './components/About';
 import Skills from './components/Skills';
-import Experience from './components/Experience';
 import Projects from './components/Projects';
 import Achievements from './components/Achievements';
 import Contact from './components/Contact';
@@ -17,7 +16,6 @@ function App() {
         <Hero />
         <About />
         <Skills />
-        <Experience />
         <Projects />
         <Achievements />
         <Contact />
